d291c197b4f852286416467137dc00a1e25083c2
aliaksandraRybak <aliaksandra.rybak@gmail.com>
2018-09-24 02:34:10 +0300

    task 7 zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	var zeros = 0;
+
+	for (var i = 1; i <= number; i++) {
+		if (i % 5 == 0) zeros += checkFives(i);
+	}
+
+	function checkFives(num) {
+		var fives = 0;
+
+		if (num % 5 == 0) {
+			fives++;
+			fives += checkFives(num/5);
+		}
+
+		return fives;
+	}
+
+	return zeros;
 }
